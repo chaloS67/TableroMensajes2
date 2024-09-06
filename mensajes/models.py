0dda5573251940df16e1dbcdd178e05d4dfb8666
gonzalo <chalosampini@hotmail.com>
2024-09-06 13:13:53 -0300

    agrege modelo y forms

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tablero(models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=50)
+    destinatario = models.CharField(max_length=50)
+    
+def __str__(self):
+    return self.titulo
--- mensajes/views.py
+++ mensajes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .forms import TableroForm
 
 # Create your views here.
 
 def home_view (request):
-    return render(request,'home.html')
+    tablero_form = TableroForm()
+    return render(request,'home.html',{'form':tablero_form})
 
 def eliminar_view (request):
     return render (request,'eliminar.html')
